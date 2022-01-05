abddda26b8bf71bad3035e23ae71b3bfc259eef0
oliverjam <oliverjamesphillips@gmail.com>
2022-01-04 16:42:52 -0800

    Extract post retrieval to model

--- functions/blog/[slug]/index.js
+++ functions/blog/[slug]/index.js
@@ -1,9 +1,8 @@
 import { html, send } from "../../../lib/helpers.js";
+import { get } from "../../../lib/model/post.js";
 
 export async function onRequestGet({ params, env }) {
-  let post = await env.DB.getWithMetadata(`post::${params.slug}`, {
-    type: "json",
-  });
+  let post = await get(env.DB, params.slug);
   console.log(post);
   let title = params.slug;
   let body = html` <h1>${title}</h1> `;
