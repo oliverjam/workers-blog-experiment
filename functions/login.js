f85c1534d40279954763997842971a50c03ed6ae
oliverjam <oliverjamesphillips@gmail.com>
2022-01-05 13:58:51 -0800

    Improve login error messaging

--- functions/login.js
+++ functions/login.js
@@ -1,25 +1,42 @@
+import { parse } from "../lib/cookie.js";
 import { html, redirect, send } from "../lib/helpers.js";
 import { create } from "../lib/model/session.js";
 
-export function onRequestGet() {
+export function onRequestGet({ request }) {
+  let cookies = parse(request.headers.get("cookie"));
+  let flash = cookies && cookies.get("flash");
   let title = "Log in";
   let body = html`
     <h1>${title}</h1>
     <form method="POST">
       <label for="pw">Admin password</label>
-      <input type="password" id="pw" name="pw" />
-
+      <input
+        type="password"
+        id="pw"
+        name="pw"
+        aria-describedby="pw_error"
+        required
+      />
+      ${flash === "pw_error"
+        ? html`<p id="pw_error" style="color: red">Password does not match</p>`
+        : ""}
       <button>Log in</button>
     </form>
   `;
-  return send(title, body);
+  let response = send(title, body);
+  if (flash) {
+    let cookie = `flash=0; Path=/; HttpOnly; MaxAge=0`;
+    response.headers.append("set-cookie", cookie);
+  }
+  return response;
 }
 
 export async function onRequestPost({ request, env }) {
   let body = await request.formData();
   let pw = body.get("pw");
   if (pw !== env.ADMIN_PW) {
-    return redirect("");
+    let cookie = `flash=pw_error; Path=/; HttpOnly`;
+    return redirect("/login", { headers: { "set-cookie": cookie } });
   } else {
     let sid = await create(env.DB, JSON.stringify({ role: "admin" }));
     let one_week = 1000 * 60 * 60 * 24 * 7;
